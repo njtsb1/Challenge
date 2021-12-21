b0944b9df8e5802829948b9d7ace448a167c886f
njtsb1 <95108889+njtsb1@users.noreply.github.com>
2021-12-20 21:35:11 -0300

    Update Desafio_2.cs

--- Desafio_2.cs
+++ Desafio_2.cs
@@ -1,3 +1,16 @@
+// Challenge
+// Two motorcycles (X and Y) depart in the same direction. The X bike leaves with a constant speed of 60 km/h and the Y bike leaves with a constant speed of 90 km/h.
+
+// In one hour (60 minutes) the bike Y can distance itself 30 kilometers from the bike X, that is, it can move away one kilometer every 2 minutes.
+
+// Your challenge is to read the distance (in km) and calculate how long it takes (in minutes) for bike Y to take that distance from the other bike.
+
+// Input
+// The input file contains an integer K that represents how many kilometers the bike Y must be from the bike X.
+
+// Exit
+// Print the time required for the bike Y to stay with the K kilometer quantity of the bike X, followed by the message "minutes".
+
 using System;
 
 class Desafio {
