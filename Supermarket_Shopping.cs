cd32ee583583bd63944e5d22f59c515a9e81c394
Nivaldo Beirão <95108889+njtsb1@users.noreply.github.com>
2022-02-28 19:15:22 -0300

    Update Supermarket_Shopping.cs

--- Supermarket_Shopping.cs
+++ Supermarket_Shopping.cs
@@ -2,19 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-class ComprasNoSupermercado
+class Challenge
 {
   public static void Main(string[] args)
   {
-    int numeroDeTestes = int.Parse(Console.ReadLine());
+    int numberOftests = int.Parse(Console.ReadLine());
   
-    for (int i = 0; i < numeroDeTestes; i++) {
-      List<string> listDeCompras = new List<String>(Console.ReadLine().Split(' '));
-      List<string> listaSemDuplicados = listDeCompras.Distinct().ToList();
+    for (int i = 0; i < numberOftests; i++) {
+      List<string> shoppinglist = new List<String>(Console.ReadLine().Split(' '));
+      List<string> listWithoutDuplicates = shoppinglist.Distinct().ToList();
 
-      listaSemDuplicados.Sort();
+      listWithoutDuplicates.Sort();
   
-      foreach (string item in listaSemDuplicados) {
+      foreach (string item in listWithoutDuplicates) {
         Console.Write($"{item} ");
       }
       
